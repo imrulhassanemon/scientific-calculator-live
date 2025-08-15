039b5a7fd78df01ecb74846298f955c3cebd0b02
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-08-15 23:52:48 +0600

    added some expression

--- src/script.js
+++ src/script.js
@@ -16,7 +16,17 @@ const arr = Array.from(buttons)
 
 // }
 const evaluateResult = () => {
-    const convertedValue = currentValue.replace(/x/g , '*').replace('÷', '/'); // Replace all 'x'
+    const convertedValue = currentValue
+    .replace(/x/g , '*')
+    .replace(/÷/g, '/')
+    .replace('%', '*0.01')
+    .replace('Sin', 'Math.sin')
+    .replace('ln', 'Math.log')
+    .replace('π', 'Math.PI')
+    .replace('cos', 'Math.cos')
+    .replace('e', 'Math.E')
+    .replace('log', 'Math.log10')
+    ; // Replace all 
     const valueAfterEval = eval(convertedValue);
     const result = valueAfterEval.toString();
     display.value = result;
